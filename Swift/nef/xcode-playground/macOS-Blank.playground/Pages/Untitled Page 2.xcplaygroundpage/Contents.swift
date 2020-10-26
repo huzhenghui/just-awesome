d304b8c55512e5647f1eb6b00a840a13e95caa89
胡争辉 <hu@daonao.com>
2020-10-26 17:00:36 +0800

    remove nef/macos transform blank

--- Swift/nef/xcode-playground/macOS-Blank.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: [Previous](@previous)
-
-//: Page 2
-
-import Foundation
-
-var str = "Hello, playground"
-
-//: [Next](@next)
--- Swift/nef/xcode-playground/macOS-Blank.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++
@@ -1,5 +0,0 @@
-//: # Page 1
-
-import Cocoa
-
-var str = "Hello, playground"
